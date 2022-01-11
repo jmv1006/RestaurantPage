464595f80bf2f434bed28f1852d0b3fddc27ee8b
Jonathan Andrade Valenzuela <jmv1006@gmail.com>
2022-01-10 21:32:38 -0800

    2nd commit

--- src/homepage.js
+++ src/homepage.js
@@ -1,25 +1,38 @@
 
 const contentModule = document.getElementById('content');
 
+//<-- Header -->
 function createHeader() {
+    //creates header container
     const homePageHeaderContainer = document.createElement('div');
     contentModule.appendChild(homePageHeaderContainer);
     homePageHeaderContainer.classList = 'header';
     
+    //container for header title
+    const headerTitle = document.createElement('div')
+    homePageHeaderContainer.appendChild(headerTitle).innerHTML = 'The Restaurant';
+    headerTitle.classList = 'title';
+
+    //container for nav items
+    const headerNavItems = document.createElement('div');
+    homePageHeaderContainer.appendChild(headerNavItems).classList = 'navItems';
+
+    //generate Nav items
     for (let i=0; i < 3; i++) {
         if (i == 0) {
             const homeButtonDiv = document.createElement('div')
-            homePageHeaderContainer.appendChild(homeButtonDiv).innerText = 'Menu';
+            headerNavItems.appendChild(homeButtonDiv).innerText = 'Home';
         } else if (i == 1) {
             const menuButtonDiv = document.createElement('div')
-            homePageHeaderContainer.appendChild(menuButtonDiv).innerText = 'Menu';
+            headerNavItems.appendChild(menuButtonDiv).innerText = 'Menu';
         } else if (i == 2) {
             const contactButtonDiv = document.createElement('div')
-            homePageHeaderContainer.appendChild(contactButtonDiv).innerText = 'Menu';
+            headerNavItems.appendChild(contactButtonDiv).innerText = 'Contact';
         }
     };
 };
 
+//<-- Main Info Area -->
 function createMainContent() {
     const img = document.createElement('img');
     img.src = '../src/images/mexicanfood.jpeg';
@@ -27,6 +40,8 @@ function createMainContent() {
     contentModule.appendChild(homePageDescription).innerText = 'Main Info';
     contentModule.appendChild(img);
 }
+
+//Loads page
 function pageLoad() {
     createHeader();
     createMainContent();
