ba119d3e3145fec7c598dc389a380ef7f2b8437e
Jonathan Andrade Valenzuela <jmv1006@gmail.com>
2022-01-11 20:40:19 -0800

    updated styling

--- src/homepage.js
+++ src/homepage.js
@@ -57,8 +57,30 @@ function createMainContent() {
     const homePageMainImage = document.createElement('div');
     homePageMainContainer.appendChild(homePageMainImage);
     homePageMainImage.id = 'homePageMainImage';
-    homePageMainImage.style.backgroundImage = "url(../src/images/background.jpeg)"
+    homePageMainImage.style.backgroundImage = "url(../src/images/background.jpg)"
+    homePageMainImage.innerHTML = 'Gourmet Dining At Sea'
 
+    const homePageInfoContainer = document.createElement('div');
+    homePageMainContainer.appendChild(homePageInfoContainer);
+    homePageInfoContainer.id = "homePageInfo";
+
+    const aboutHeader = document.createElement('div');
+    homePageInfoContainer.appendChild(aboutHeader);
+    aboutHeader.id = 'aboutHeader';
+    aboutHeader.innerHTML = 'THE MOST GOURMET DINING EXPERIENCE AT SEA WITH A TOUCH OF MAGIC';
+
+    const aboutParagraph = document.createElement('div');
+    homePageInfoContainer.appendChild(aboutParagraph);
+    aboutParagraph.id = "aboutParagraph"
+    aboutParagraph.innerHTML = `The Restaurant was created in order to give Disney Cruise Line guests 
+    a gourmet dining experience without having to leave the magic. Our chefs from around the world pride 
+    themselves on their world class cuisine that is sure to leave you wanting more. Our cuisine has deep roots
+    in the traditional cuisines of Central & South America, with a modern twist!`
+
+    const reservationButton = document.createElement('button');
+    homePageInfoContainer.appendChild(reservationButton);
+    reservationButton.id = "homePageResBtn";
+    reservationButton.innerHTML = 'Make Reservations';
 };
 
 
